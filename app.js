3702877fba9976c90a9337d97fb67d7b62fd0305
Smxkez <sandu.albert.alexandru@gmail.com>
2022-08-29 21:14:36 +0300

    Changed header favicon

--- app.js
+++ app.js
@@ -137,6 +137,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started.");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
